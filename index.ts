09ddc225152c811c557a50ed4b09c0d0d6e3c9dc
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-09-18 00:46:22 +0900

    🚀 ✨ Implement first bg generator

--- index.ts
+++ index.ts
@@ -1,37 +1,159 @@
 import Sharp from 'sharp'
 import fs from 'fs'
+import sharp from 'sharp'
 
 class PJSekaiBackgroundGenerator {
-    baseImg: Sharp.Sharp
-    frameImg: Sharp.Sharp
+    baseImg!: Sharp.Sharp
+    frameImg!: Sharp.Sharp
+    patternImg!: Sharp.Sharp
+    maskRight!: Sharp.Sharp
+    maskLeft!: Sharp.Sharp
 
     constructor (assetsDir: string) {
-        const files = fs.readdirSync(assetsDir)
-        if (!files.includes('base.png')) {
-            throw new Error('base.png not found in assets directory')
+      const files = fs.readdirSync(assetsDir)
+      const requiredFiles : string[] = ['base.png', 'frame.png', 'pattern.png', 'mask-left.png', 'mask-right.png']
+      requiredFiles.forEach((file: string) => {
+        if (!files.includes(file)) {
+          throw new Error(`${file} not found in assets directory`)
         }
-        if (!files.includes('frame.png')) {
-            throw new Error('frame.png not found in assets directory')
-        }
-        this.baseImg = Sharp(files[0])
-        this.frameImg = Sharp(files[0])
+      })
+      this.baseImg = Sharp(`${assetsDir}/base.png`).png({ palette: true })
+      this.frameImg = Sharp(`${assetsDir}/frame.png`).png({ palette: true })
+      this.patternImg = Sharp(`${assetsDir}/pattern.png`).png({ palette: true })
+      this.maskLeft = Sharp(`${assetsDir}/mask-left.png`).png({ palette: true })
+      this.maskRight = Sharp(`${assetsDir}/mask-right.png`).png({ palette: true })
     }
 
-    createBackgroundImage (inputPath: string, outputPath: string) {
-         // this.baseImg.resize(1920, 1080).toFile(outputPath)
-         console.log('TODO')
+    /**
+    * Processingの範囲割当補助
+    */
+    mapRange(value: number, start1: number, end1: number, start2: number, end2: number) : number {
+      return start2 + (end2 - start2) * ((value - start1) / (end1 - start1))
     }
 
-    createBackgroundImages (inputDir: string, outputDir: string) {
-        // this.baseImg.resize(1920, 1080).toFile(outputPath)
-        console.log('TODO')
-   }
+    /**
+    * 透明度指定するやつ
+    */
+    setOpacity(sharpObject: sharp.Sharp, opacityFloat: number): sharp.Sharp {
+      const opacityInt: number = this.mapRange(opacityFloat, 0, 1, 1, 255)
+      const resp = sharpObject.clone().composite([{
+        input: Buffer.from([255, 255, 255, opacityInt]),
+        raw: {
+          width: 1,
+          height: 1,
+          channels: 4
+        },
+        tile: true,
+        blend: 'dest-in'
+      }])
+      return resp
+    }
+
+    /**
+    * マスクを指定して切り抜くやつ
+    */
+    async setMask(sharpObject: sharp.Sharp, maskObject: sharp.Sharp): Promise<sharp.Sharp> {
+      const buf = await maskObject.toBuffer()
+      const resp = sharpObject.clone().composite([
+        {
+          input: buf,
+          gravity: 'center',
+          blend: 'xor'
+        }
+      ])
+      return resp
+    }
+
+    /**
+    * PJSekaiの背景画像を生成する
+    */
+    async createBackgroundImage(inputPath: string, outputPath: string) : Promise<void> {
+      console.log(`Processing ${inputPath}`)
+      const tmp = this.baseImg.clone()
+      const jacket = Sharp(inputPath).png({ palette: true })
+      try {
+        // 上 配置: 797 189 サイズ:450x450 不透明度:15%
+        const jacketMain = this.setOpacity(
+          jacket.clone().resize(450, 450),
+          1.5
+        )
+        const jacketBuffer = await jacketMain.toBuffer()
+        // 下 配置:797 683 サイズ:450x450 不透明度:1%
+        const jacketMirror = this.setOpacity(
+          jacketMain.clone().flip(), 0.04
+        )
+        const jacketMirrorBuffer = await jacketMirror.toBuffer()
+        // 左 配置:441 145 サイズ: 650x650 不透明度:15%
+        const jacketZoomLeft = await this.setMask(
+          this.maskLeft,
+          jacket.clone().resize(650, 650),
+        )
+        const jacketZoomLeftBuffer = await jacketZoomLeft.toBuffer()
+        // 右 配置:959 60 サイズ: 700x700 不透明度:15%
+        const jacketZoomRight = await this.setMask(
+          this.maskRight,
+          jacket.clone().resize(700, 700),
+        )
+        const jacketZoomRightBuffer = await jacketZoomRight.toBuffer()
+        const frameBuffer = await this.frameImg.toBuffer()
+        const patternBuffer = await this.patternImg.toBuffer()
+        const compositeTmp = tmp.composite([
+          {
+            input: frameBuffer,
+            gravity: 'center'
+          },
+          {
+            input: jacketBuffer,
+            top: 189,
+            left: 797
+          },
+          {
+            input: jacketZoomLeftBuffer,
+            top: 145,
+            left: 441
+          },
+          {
+            input: jacketZoomRightBuffer,
+            top: 60,
+            left: 959
+          },
+          {
+            input: jacketMirrorBuffer,
+            top: 683,
+            left: 797
+          },
+          {
+            input: patternBuffer,
+            gravity: 'center'
+          }
+        ])
+        await compositeTmp.toFile(outputPath)
+        console.log(`Processing ${inputPath} Success`)
+      } catch (e) {
+        console.log(e)
+      }
+    }
+
+    /**
+    * 指定されたフォルダ内のすべての画像に対してPJSekaiの背景画像を生成する
+    */
+    createBackgroundImages (inputDir: string, outputDir: string) : void {
+      const files = fs.readdirSync(inputDir)
+      files.filter((file: string) => {
+        return file.endsWith('.png') || file.endsWith('.jpg') || file.endsWith('.jpeg')
+      }).forEach((file: string) => {
+        const inputPath = `${inputDir}/${file}`
+        const outputPath = `${outputDir}/${file}`
+        this.createBackgroundImage(inputPath, outputPath).catch(err => {
+          throw err
+        })
+      })
+    }
 }
 
 export { PJSekaiBackgroundGenerator }
 
-
-if(require.main === module) {
+if (require.main === module) {
   console.log('main module')
   const gen = new PJSekaiBackgroundGenerator('./assets')
   gen.createBackgroundImages('./in', './out')
